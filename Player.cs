4bb5921e3c11f4ae773de8e565fd8e53ec10ccd6
Eric Lin <eric.lin@titansoft.com.sg>
2024-04-03 17:18:27 +0800

    add property

--- Player.cs
+++ Player.cs
@@ -2,9 +2,15 @@ using Godot;
 
 public partial class Player : Area2D
 {
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+
+	public Vector2 ScreenSize; // Size of the game window.
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
